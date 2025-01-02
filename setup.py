a7158b882f091c3c6619c7a71fa0993d6693ee5a
Falk B. Schimweg <git@falk.schimweg.de>
2025-01-02 22:44:58 +0100

    Make version dynamic

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="uuid-filelock",
     description="A simple UUID-based file lock implementation for Python.",
+    use_scm_version=True,
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="Falk B. Schimweg",
@@ -24,6 +25,7 @@ setup(
             "pytest>=6.0",
         ]
     },
+    setup_requires=['setuptools_scm'],
     python_requires=">=3.7",
 )
 
